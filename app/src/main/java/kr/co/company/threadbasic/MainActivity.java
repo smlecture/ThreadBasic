7225c4d23c25772abfc82b99f7d21a30d5902bb6
H-J Yoo <yoohj@smu.ac.kr>
2021-05-31 23:05:10 +0900

    Runnable as anonymous inner class

--- app/src/main/java/kr/co/company/threadbasic/MainActivity.java
+++ app/src/main/java/kr/co/company/threadbasic/MainActivity.java
@@ -7,24 +7,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    WorkerThread wt;
     Thread wr;
 //    WorkerRunnable wr;
     boolean running = true;
     String strTag = "THREAD";
-
-    class WorkerThread extends Thread {
-        public void run() {
-            int i = 0;
-            for (i = 0; i < 20 && running; i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                }
-                Log.v(strTag, "Thread time=" + i);
-            }
-        }
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        wt = new WorkerThread();
-//        wr = new WorkerRunnable();
         wr = new Thread(new Runnable() {
             public void run() {
                 int i = 0;
@@ -53,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         running = true;
         wr.start();
 
-        wt.start();
         Log.v(strTag, "Now I am in onStart");
     }
 
