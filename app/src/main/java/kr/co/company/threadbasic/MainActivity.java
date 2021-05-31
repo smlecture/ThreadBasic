62beff99f00f4ebb25fcfe6a17e7d948dfe82ca1
H-J Yoo <yoohj@smu.ac.kr>
2021-05-31 22:50:39 +0900

    added Runnable as separate class (2)

--- app/src/main/java/kr/co/company/threadbasic/MainActivity.java
+++ app/src/main/java/kr/co/company/threadbasic/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     WorkerThread w;
+    Thread wr;
     boolean running = true;
     String strTag = "THREAD";
 
@@ -19,11 +20,23 @@ public class MainActivity extends AppCompatActivity {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                 }
-                Log.v(strTag, "time=" + i);
+                Log.v(strTag, "Thread time=" + i);
             }
         }
     }
+    class WorkerRunnable implements Runnable{
+        public void run() {
+            int i = 0;
+            for (i = 0; i < 20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                Log.v("THREAD", "Runnable time=" + i);
+            }
+        }
 
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         w = new WorkerThread();
+        wr = new Thread(new WorkerRunnable());
         running = true;
         w.start();
+        wr.start();
         Log.v(strTag, "Now I am in onStart");
     }
 
